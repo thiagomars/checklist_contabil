48cfdcd6b5194b5a63ca73309aff972e184fa749
thiagomars <r15.thiagomarques@gmail.com>
2023-09-25 01:39:41 -0300

    correcao

--- src/components/Inputs/index.tsx
+++ src/components/Inputs/index.tsx
@@ -1,26 +1,27 @@
-import * as Form from '@radix-ui/react-form';
+// import * as Form from '@radix-ui/react-form';
 
-export default function Formulario( children : JSX.Element ) {
+export default function Formulario( ) {
 
 
     return (
-        <Form.Root>
-            <Form.Field>
-                <div className="flex items-baseline justify-between">
-                    <Form.Label className="text-[15px] font-medium leading-[35px] text-white">Email</Form.Label>
-                    <Form.Message className="text-[13px] text-white opacity-[0.8]" match="valueMissing">
-                        Please enter your email
-                    </Form.Message>
-                    <Form.Message className="text-[13px] text-white opacity-[0.8]" match="typeMismatch">
-                        Please provide a valid email
-                    </Form.Message>
-                </div>
+        // <Form.Root>
+        //     <Form.Field>
+        //         <div className="flex items-baseline justify-between">
+        //             <Form.Label className="text-[15px] font-medium leading-[35px] text-white">Email</Form.Label>
+        //             <Form.Message className="text-[13px] text-white opacity-[0.8]" match="valueMissing">
+        //                 Please enter your email
+        //             </Form.Message>
+        //             <Form.Message className="text-[13px] text-white opacity-[0.8]" match="typeMismatch">
+        //                 Please provide a valid email
+        //             </Form.Message>
+        //         </div>
 
-                <Form.Control>
-                    { children }
-                </Form.Control>
+        //         <Form.Control>
+        //             { children }
+        //         </Form.Control>
 
-            </Form.Field>
-        </Form.Root>
+        //     </Form.Field>
+        // </Form.Root>
+        <></>
     )
 }
